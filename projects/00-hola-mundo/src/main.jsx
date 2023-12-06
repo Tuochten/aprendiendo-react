3185a45625dc8ba18cab9f38b0ed9313229ce252
Tuochten <tuochten@gmail.com>
2023-12-05 21:27:49 -0300

    parte 1
    creación de módulos y añadir un css básico

--- projects/00-hola-mundo/src/main.jsx
+++ projects/00-hola-mundo/src/main.jsx
@@ -1,10 +1,19 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import { App } from './App'
 import './index.css'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+/* const SpecialButton = ({text}) => {
+  return (
+    <button>{text}</button>
+    )
+  } */
+  
+
+const root = ReactDOM.createRoot(document.getElementById('root'))
+
+root.render(
+  <React.Fragment>
     <App />
-  </React.StrictMode>,
+  </React.Fragment>
 )
